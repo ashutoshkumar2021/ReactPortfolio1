27a3a5d7996d3eb571df6d04670cd353ba1a43cd
Ashutosh Kumar <80633589+ashutoshkumar2021@users.noreply.github.com>
2023-06-17 21:27:21 +0530

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -30,7 +30,7 @@ const Home = () => {
             <div>
                 <br/>
                 <br/>
-                <img src={heroImage} style="Shadow-blue-500" alt="My profile" className='rounded-2xl mx-auto w-2/3 md:w-full'/>
+                <img src={heroImage} alt="My profile" className='rounded-2xl mx-auto w-2/3 md:w-full'/>
             </div>
         </div>
     </div>
